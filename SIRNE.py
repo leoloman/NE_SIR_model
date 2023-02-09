338f96d88e63096c10e8aee007152d96b5a288a4
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-02-09 10:20:37 +0000

    added static semi random network

--- SIRNE.py
+++ SIRNE.py
@@ -13,9 +13,9 @@ class SIRNE:
     def __init__(self, 
                  r,
                  mu,
-                 ro,
                  epsilon,
-                 time, 
+                 time,
+                 ro = None,
                  calc_g = None,
                  calc_g1 = None,
                  calc_g2 = None, 
@@ -23,14 +23,14 @@ class SIRNE:
                  G = None,
                  degree_dist = None):
         """
-        Instantiate the Volz/Mercer Neighbour Exchange SIR Model
+        Instantiate the Volz Semi Random Static Network (VN) or Volz/Mercer Neighbour Exchange SIR Model (NE)
         
         args
             r: float - disease transmission to neightbor at a constant rate
             mu: float - infectious indivduals recover at a constant rate
-            ro: float - rate at which neighbours are exchanged
             epsilon - fraction of infectious nodes 
             time: int - total steps 
+            ro: float - rate at which neighbours are exchanged (OPTIONAL for VN, NEEDED for NE model)
             calc_g: types.FunctionType - function representing a probability generating function 
             calc_g1: types.FunctionType - function representing a probability generating function's first derivative 
             calc_g2: types.FunctionType - function representing a probability generating function's second derivative
@@ -66,7 +66,7 @@ class SIRNE:
             self.calc_g2 = lambda x: calc_g2(x, probability_lambda)
         
         # set the initial values
-        self.initial_state = [1 - epsilon, # 
+        self.dynamic_initial_state = [1 - epsilon, # 
            epsilon / (1 - epsilon), # 
            (1 - 2*epsilon)/(1 - epsilon), #
            self.calc_g(1 - epsilon),  # Susceptible
@@ -74,17 +74,31 @@ class SIRNE:
            1 - self.calc_g(1- epsilon) # Infected
            ]
     
+        self.static_initial_state = [1 - epsilon, # theta
+       epsilon / (1 - epsilon), #force of infection 
+       (1 - 2*epsilon)/(1 - epsilon), # represents the probability that an arc with a susceptible ego has a susceptible alter
+       calc_g( 1 - epsilon), #
+       1 - calc_g( 1- epsilon) # 
+       ]
+    
         self.time = list(range(time))
     
-    def run_simulation(self):
+    def run_dynamic_simulation(self):
         """
         Run a single simulation using scipy odeint function
         """
-        self.out = sp_int.odeint(self.derv, self.initial_state, self.time, args=(self.r, self.mu, self.ro, 
+        self.dyn_out = sp_int.odeint(self.dynamic_ode, self.initial_state, self.time, args=(self.r, self.mu, self.ro, 
+                                          self.calc_g, self.calc_g1, self.calc_g2))
+        
+    def run_static_simulation(self):
+        """
+        Run a single simulation using scipy odeint function
+        """
+        self.static_out = sp_int.odeint(self.static_semi_random, self.static_initial_state, self.time, args=(self.r, self.mu,  
                                           self.calc_g, self.calc_g1, self.calc_g2))
     
 
-    def derv(self, x,t,rr,mm,pp, calc_g, calc_g1, calc_g2):
+    def dynamic_ode(self, x,t,rr,mm,pp, calc_g, calc_g1, calc_g2):
         #y[0]= change of theta 
         #y[1]= change of p_infec 
         #y[2]= change of p_suscep 
@@ -98,5 +112,24 @@ class SIRNE:
         y[3]=-rr*x[1]*x[0]*calc_g1(x[0]) 
         y[4]=-mm*x[4]+rr*x[1]*(x[0]**2*calc_g2(x[0])+x[0]*calc_g1(x[0])/calc_g1(1)) 
         y[5]=rr*x[1]*x[0]*calc_g1(x[0])-mm * x[5]
+        return(y)
+    
+    def static_semi_random(self, x, t, rr, mm, calc_g, calc_g1, calc_g2):
+        #x[0] - theta
+        # x[1] pi
+        #x[2] ps
+        # x[3] s
+        #x[4] I
+
+        y=list(range(5))#zeros(6);
+
+        y[0]= -rr * x[1] * x[0] 
+
+        y[1]=rr * x[2] * x[1] * x[0] * (calc_g2(x[0])/calc_g1(x[0])) - rr * x[1] * (1 - x[1]) - x[1] * mm
+
+        y[2]= rr * x[2] * x[1] * ( 1- x[0] * (calc_g2(x[0]) / calc_g1(x[0]) ) )
+
+        y[3]= -rr * x[1] * x[0] * calc_g1(x[0])
+        y[4] = rr * x[1] * x[0] * calc_g1(x[0]) - mm * x[4]
         return(y)
     
