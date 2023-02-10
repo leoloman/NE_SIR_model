514aec06c15d2287e9cc5fdb2e759711018eeeb2
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-02-10 21:41:08 +0000

    split the parent class from the child classes

--- SIRNE.py
+++ SIRNE.py
@@ -8,87 +8,6 @@ import numpy as np
 import networkx as nx
 import types
 
-
-class VolzFramework:
-    """
-    args
-        epsilon - fraction of infectious nodes
-        time: int - total steps
-        calc_g: types.FunctionType - function representing a probability generating function
-        calc_g1: types.FunctionType - function representing a probability generating function's first derivative
-        calc_g2: types.FunctionType - function representing a probability generating function's second derivative
-        probability_lambda: - variable which may be requried as a second parameter in the probability generating function
-        G: nx.Graph - a network to obtain a probability generating function from
-        degree_dist: dict - a dictionary of degree distributions, where the key is the degree and the value is the probability - this will be used to construct a probability generating function
-    """
-
-    def __init__(
-        self,
-        epsilon,
-        time,
-        calc_g=None,
-        calc_g1=None,
-        calc_g2=None,
-        probability_lambda=None,
-        G=None,
-        degree_dist=None,
-    ):
-        self.epsilon = epsilon
-
-        if isinstance(G, nx.Graph):
-            degree_dist = pgf.get_Pk(G)
-            self.calc_g = pgf.get_PGF(degree_dist)
-            self.calc_g1 = pgf.get_PGF_first_derivate(degree_dist)
-            self.calc_g2 = pgf.get_PGF_second_derivate(degree_dist)
-
-        elif isinstance(degree_dist, dict):
-            self.calc_g = pgf.get_PGF(degree_dist)
-            self.calc_g1 = pgf.get_PGF_first_derivate(degree_dist)
-            self.calc_g2 = pgf.get_PGF_second_derivate(degree_dist)
-
-        elif (
-            isinstance(calc_g, types.FunctionType)
-            & isinstance(calc_g1, types.FunctionType)
-            & isinstance(calc_g2, types.FunctionType)
-            & (probability_lambda == None)
-        ):
-            self.calc_g = calc_g
-            self.calc_g1 = calc_g1
-            self.calc_g2 = calc_g2
-
-        elif (
-            isinstance(calc_g, types.FunctionType)
-            & isinstance(calc_g1, types.FunctionType)
-            & isinstance(calc_g2, types.FunctionType)
-            & (
-                isinstance(probability_lambda, float)
-                | isinstance(probability_lambda, int)
-                | isinstance(probability_lambda, list)
-            )
-        ):
-            self.calc_g = lambda x: calc_g(x, probability_lambda)
-            self.calc_g1 = lambda x: calc_g1(x, probability_lambda)
-            self.calc_g2 = lambda x: calc_g2(x, probability_lambda)
-
-        self._set_intial_states()
-
-    def _set_inital_states(self):
-        """
-        Placeholder: Set the initial states
-        """
-
-    def run_simulation(self):
-        """
-        Placeholder: Run a single simulation using scipy odeint function
-        """
-        pass
-
-    def ode(self):
-        """
-        Placeholder: function to hold all the equations in
-        """
-
-
 class SIRNE(VolzFramework):
     """
     Instantiate the  Volz/Mercer Neighbour Exchange SIR Model (NE)
@@ -162,17 +81,18 @@ class SIRSR(VolzFramework):
     __doc__ += VolzFramework.__doc__
 
     def _set_inital_states(self):
-        """ """
+        """ 
+        """
         # set the initial values
         self.initial_state = [
-            1 - epsilon,  # theta
-            epsilon / (1 - epsilon),  # force of infection
-            (1 - 2 * epsilon)
+            1 - self.epsilon,  # theta
+            self.epsilon / (1 - self.epsilon),  # force of infection
+            (1 - 2 * self.epsilon)
             / (
-                1 - epsilon
+                1 - self.epsilon
             ),  # represents the probability that an arc with a susceptible ego has a susceptible alter
-            self.calc_g(1 - epsilon),  #
-            1 - self.calc_g(1 - epsilon),  #
+            self.calc_g(1 - self.epsilon),  #
+            1 - self.calc_g(1 - self.epsilon),  #
         ]
 
     def run_simulation(self, r: float, mu: float) -> np.array:
